08adc5534bb2f4b2bad73386ab1d5a41d1858e2d
jeongsooyoung <code.ameba@gmail.com>
2020-02-27 22:58:29 +0900

    add realtime clock javascript

--- js/clock.js
+++ js/clock.js
@@ -1,14 +1,12 @@
-const clockContainer = document.querySelector(".status-bar__clock"),
+const statusClock = document.querySelector('.status-bar__clock');
 
 function getTime() {
     const date = new Date();
     const hours = date.getHours();
     const minutes = date.getMinutes();
-    const seconds = date.getSeconds();
-    clockContainer.innerHTML = 
-    `${hours < 10 ? `0${hours}` : hours}:
-    ${minutes < 10 ? `0${minutes}` : minutes}:
-    ${seconds < 10 ? `0${seconds}` : seconds}`;
+    statusClock.innerHTML = 
+    `${hours < 10 ? `0${hours}` : hours} :
+    ${minutes < 10 ? `0${minutes}` : minutes}`;
 }
 
 function init() {
